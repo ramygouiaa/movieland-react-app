87a35b308360d214d0d202dfd15add77a963261a
Gouiaa, Ramy <ramy.gouiaa@hs-ruhrwest.de>
2023-10-24 17:25:56 +0100

    code cleaning and updating request url

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import './App.css';
 import SearchIcon from './search.svg';
 import MovieCard from './MovieCard';
@@ -8,38 +8,18 @@ export default function App() {
   //http://www.omdbapi.com/?i=tt3896198&apikey=fc73afe7
   //http://www.omdbapi.com/?i=tt3896198&apikey=ef9e4d4e
 
-  const API_URL = `http://www.omdbapi.com/?apikey=fc73afe7&s=`;
+  const API_URL = `http://localhost:3000/api/v1/movie/`;
   // State variables to store the search term and results
   const [movies, setMovies] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
-
-  //test Data 
-  const movie1 = {
-    "Title": "Spiderman and Grandma",
-    "Year": "2009",
-    "imdbID": "tt1433184",
-    "Type": "movie",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BMjE3Mzg0MjAxMl5BMl5BanBnXkFtZTcwNjIyODg5Mg@@._V1_SX300.jpg"
-  }
-
-  const movie2 = {
-    "Title": "Spiderman",
-    "Year": "2010",
-    "imdbID": "tt1785572",
-    "Type": "movie",
-    "Poster": "N/A"
-  }
 
   const searchMovies = async (title) => {
 
     const response = await fetch(`${API_URL}${title}`);
     const data = await response.json();
-    console.log(data.Search);
     setMovies(data.Search);
 
   }
-
-  //useEffect(() => {searchMovies('Spiderman')}, [])
 
   return (
     <div className="app">
